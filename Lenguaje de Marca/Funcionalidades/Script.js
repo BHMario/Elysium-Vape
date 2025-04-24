a12de1212adf7ba4e639a0d2bb1f8fcab406d878
francopdl <francoopdl@gmail.com>
2025-04-24 19:22:55 +0200

    Seccion vaper añadido

--- Lenguaje de Marca/Funcionalidades/Script.js
+++ Lenguaje de Marca/Funcionalidades/Script.js
@@ -1,16 +1,30 @@
 window.onload = () => {
-    document.getElementById("modal-edad").style.display = "flex";
+  document.getElementById("modal-edad").style.display = "flex";
 };
-  
+
 function confirmarEdad(esMayor) {
-    if (esMayor) {
+  if (esMayor) {
       document.getElementById("modal-edad").style.display = "none";
       document.getElementById("popup-cookies").style.display = "block";
-    } else {
+  } else {
       window.location.href = "https://www.google.com";
-    }
+  }
 }
-  
+
 function aceptarCookies() {
-    document.getElementById("popup-cookies").style.display = "none";
+  document.getElementById("popup-cookies").style.display = "none";
 }
+
+// Actualización en tiempo real del valor de nicotina
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.getElementById("nicotina");
+  const output = document.getElementById("valor-nicotina");
+
+  // Inicializa el valor mostrado
+  output.textContent = `${slider.value} mg`;
+
+  // Cambia el valor a medida que se mueve la barra
+  slider.addEventListener("input", function () {
+      output.textContent = `${slider.value} mg`;
+  });
+});
